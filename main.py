d1199ff503ab2f0a89a2138167f23e23334d4bef
Amarnath Reddy <73329067+embeddeddeveloper12@users.noreply.github.com>
2023-11-12 18:21:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+import requests as r
+nme=[]
+regNo=[]
+Age=[]
+st.header("Registration form")
+fullNum=st.text_input("Full Name : ")
+fatherName = st.text_input("Father name : ")
+motherName = st.text_input("Mother name : ")
+age=st.slider("Age(in years) : ",1,100)
+gender=st.radio("Gender :",["Male","Female"])
+nationality=st.selectbox("Nationality :",["Indian","other"])
+email=st.text_input("E-mail : ")
+submit=st.button("submit")
+if submit==True:
+    if len(fullNum)==0 or len(fatherName)==0 or len(motherName)==0 or len(email)==0:
+        st.error("Fill all feilds above")
+        mail=email
+        if len(mail)>0 and mail.find("@")<0:
+                st.error("Enter a valid email")
